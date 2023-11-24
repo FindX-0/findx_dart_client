5b0fc098b49da2b54bc59684e629f5a66728c21d
kume1a <kumela011@gmail.com>
2023-11-24 16:15:35 +0400

    rename math field typedefs

--- packages/gql_types/lib/src/math_field/query/index.dart
+++ packages/gql_types/lib/src/math_field/query/index.dart
@@ -4,7 +4,7 @@ import 'filter_math_fields.gql.dart';
 export 'filter_math_fields.gql.dart';
 export 'get_math_field.gql.dart';
 
-typedef GetMathFieldByIdRes = Fragment$MathField;
+typedef MathFieldGetByIdRes = Fragment$MathField;
 
-typedef MathFieldsPage = Query$FilterMathFields$filterMathFields;
-typedef MathFieldsPageItem = Fragment$MathField;
+typedef MathFieldPage = Query$FilterMathFields$filterMathFields;
+typedef MathFieldPageItem = Fragment$MathField;
