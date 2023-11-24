f7f7c0ef34a81316c8a7fb5af408d1aaa601efa1
kume1a <kumela011@gmail.com>
2023-11-24 17:50:17 +0400

    export gql types

--- lib/app_client.dart
+++ lib/app_client.dart
@@ -2,5 +2,6 @@ library;
 
 export 'package:gql_client/gql_client.dart'
     show AuthTokenStore, SecureStoreageTokenStoreImpl, NetworkClientFactory;
+export 'package:gql_types/gql_types.dart';
 
 export 'src/modules/index.dart';
