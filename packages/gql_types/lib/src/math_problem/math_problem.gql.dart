504461d37be480042331ecf4b800620ba550bade
kume1a <kumela011@gmail.com>
2023-12-04 20:13:27 +0400

    fetch images with math problem

--- packages/gql_types/lib/src/math_problem/math_problem.gql.dart
+++ packages/gql_types/lib/src/math_problem/math_problem.gql.dart
@@ -10,6 +10,7 @@ class Fragment$MathProblem {
     this.tex,
     required this.mathFieldId,
     required this.mathSubFieldId,
+    this.images,
     this.$__typename = 'MathProblemObject',
   });
 
@@ -21,6 +22,7 @@ class Fragment$MathProblem {
     final l$tex = json['tex'];
     final l$mathFieldId = json['mathFieldId'];
     final l$mathSubFieldId = json['mathSubFieldId'];
+    final l$images = json['images'];
     final l$$__typename = json['__typename'];
     return Fragment$MathProblem(
       id: (l$id as String),
@@ -30,6 +32,10 @@ class Fragment$MathProblem {
       tex: (l$tex as String?),
       mathFieldId: (l$mathFieldId as String),
       mathSubFieldId: (l$mathSubFieldId as String),
+      images: (l$images as List<dynamic>?)
+          ?.map((e) =>
+              Fragment$MathProblem$images.fromJson((e as Map<String, dynamic>)))
+          .toList(),
       $__typename: (l$$__typename as String),
     );
   }
@@ -47,6 +53,8 @@ class Fragment$MathProblem {
   final String mathFieldId;
 
   final String mathSubFieldId;
+
+  final List<Fragment$MathProblem$images>? images;
 
   final String $__typename;
 
@@ -66,6 +74,8 @@ class Fragment$MathProblem {
     _resultData['mathFieldId'] = l$mathFieldId;
     final l$mathSubFieldId = mathSubFieldId;
     _resultData['mathSubFieldId'] = l$mathSubFieldId;
+    final l$images = images;
+    _resultData['images'] = l$images?.map((e) => e.toJson()).toList();
     final l$$__typename = $__typename;
     _resultData['__typename'] = l$$__typename;
     return _resultData;
@@ -80,6 +90,7 @@ class Fragment$MathProblem {
     final l$tex = tex;
     final l$mathFieldId = mathFieldId;
     final l$mathSubFieldId = mathSubFieldId;
+    final l$images = images;
     final l$$__typename = $__typename;
     return Object.hashAll([
       l$id,
@@ -89,6 +100,7 @@ class Fragment$MathProblem {
       l$tex,
       l$mathFieldId,
       l$mathSubFieldId,
+      l$images == null ? null : Object.hashAll(l$images.map((v) => v)),
       l$$__typename,
     ]);
   }
@@ -136,6 +148,22 @@ class Fragment$MathProblem {
     if (l$mathSubFieldId != lOther$mathSubFieldId) {
       return false;
     }
+    final l$images = images;
+    final lOther$images = other.images;
+    if (l$images != null && lOther$images != null) {
+      if (l$images.length != lOther$images.length) {
+        return false;
+      }
+      for (int i = 0; i < l$images.length; i++) {
+        final l$images$entry = l$images[i];
+        final lOther$images$entry = lOther$images[i];
+        if (l$images$entry != lOther$images$entry) {
+          return false;
+        }
+      }
+    } else if (l$images != lOther$images) {
+      return false;
+    }
     final l$$__typename = $__typename;
     final lOther$$__typename = other.$__typename;
     if (l$$__typename != lOther$$__typename) {
@@ -170,8 +198,15 @@ abstract class CopyWith$Fragment$MathProblem<TRes> {
     String? tex,
     String? mathFieldId,
     String? mathSubFieldId,
+    List<Fragment$MathProblem$images>? images,
     String? $__typename,
   });
+  TRes images(
+      Iterable<Fragment$MathProblem$images>? Function(
+              Iterable<
+                  CopyWith$Fragment$MathProblem$images<
+                      Fragment$MathProblem$images>>?)
+          _fn);
 }
 
 class _CopyWithImpl$Fragment$MathProblem<TRes>
@@ -195,6 +230,7 @@ class _CopyWithImpl$Fragment$MathProblem<TRes>
     Object? tex = _undefined,
     Object? mathFieldId = _undefined,
     Object? mathSubFieldId = _undefined,
+    Object? images = _undefined,
     Object? $__typename = _undefined,
   }) =>
       _then(Fragment$MathProblem(
@@ -213,10 +249,26 @@ class _CopyWithImpl$Fragment$MathProblem<TRes>
         mathSubFieldId: mathSubFieldId == _undefined || mathSubFieldId == null
             ? _instance.mathSubFieldId
             : (mathSubFieldId as String),
+        images: images == _undefined
+            ? _instance.images
+            : (images as List<Fragment$MathProblem$images>?),
         $__typename: $__typename == _undefined || $__typename == null
             ? _instance.$__typename
             : ($__typename as String),
       ));
+
+  TRes images(
+          Iterable<Fragment$MathProblem$images>? Function(
+                  Iterable<
+                      CopyWith$Fragment$MathProblem$images<
+                          Fragment$MathProblem$images>>?)
+              _fn) =>
+      call(
+          images: _fn(
+              _instance.images?.map((e) => CopyWith$Fragment$MathProblem$images(
+                    e,
+                    (i) => i,
+                  )))?.toList());
 }
 
 class _CopyWithStubImpl$Fragment$MathProblem<TRes>
@@ -233,9 +285,12 @@ class _CopyWithStubImpl$Fragment$MathProblem<TRes>
     String? tex,
     String? mathFieldId,
     String? mathSubFieldId,
+    List<Fragment$MathProblem$images>? images,
     String? $__typename,
   }) =>
       _res;
+
+  images(_fn) => _res;
 }
 
 const fragmentDefinitionMathProblem = FragmentDefinitionNode(
@@ -297,6 +352,13 @@ const fragmentDefinitionMathProblem = FragmentDefinitionNode(
       selectionSet: null,
     ),
     FieldNode(
+      name: NameNode(value: 'images'),
+      alias: null,
+      arguments: [],
+      directives: [],
+      selectionSet: null,
+    ),
+    FieldNode(
       name: NameNode(value: '__typename'),
       alias: null,
       arguments: [],
@@ -342,4 +404,80 @@ extension ClientExtension$Fragment$MathProblem on graphql.GraphQLClient {
     );
     return result == null ? null : Fragment$MathProblem.fromJson(result);
   }
+}
+
+class Fragment$MathProblem$images {
+  Fragment$MathProblem$images();
+
+  factory Fragment$MathProblem$images.fromJson(Map<String, dynamic> json) {
+    return Fragment$MathProblem$images();
+  }
+
+  Map<String, dynamic> toJson() {
+    final _resultData = <String, dynamic>{};
+    return _resultData;
+  }
+
+  @override
+  int get hashCode {
+    return Object.hashAll([]);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (!(other is Fragment$MathProblem$images) ||
+        runtimeType != other.runtimeType) {
+      return false;
+    }
+    return true;
+  }
+}
+
+extension UtilityExtension$Fragment$MathProblem$images
+    on Fragment$MathProblem$images {
+  CopyWith$Fragment$MathProblem$images<Fragment$MathProblem$images>
+      get copyWith => CopyWith$Fragment$MathProblem$images(
+            this,
+            (i) => i,
+          );
+}
+
+abstract class CopyWith$Fragment$MathProblem$images<TRes> {
+  factory CopyWith$Fragment$MathProblem$images(
+    Fragment$MathProblem$images instance,
+    TRes Function(Fragment$MathProblem$images) then,
+  ) = _CopyWithImpl$Fragment$MathProblem$images;
+
+  factory CopyWith$Fragment$MathProblem$images.stub(TRes res) =
+      _CopyWithStubImpl$Fragment$MathProblem$images;
+
+  TRes call();
+}
+
+class _CopyWithImpl$Fragment$MathProblem$images<TRes>
+    implements CopyWith$Fragment$MathProblem$images<TRes> {
+  _CopyWithImpl$Fragment$MathProblem$images(
+    this._instance,
+    this._then,
+  );
+
+  final Fragment$MathProblem$images _instance;
+
+  final TRes Function(Fragment$MathProblem$images) _then;
+
+  static const _undefined = <dynamic, dynamic>{};
+
+  TRes call() => _then(Fragment$MathProblem$images());
+}
+
+class _CopyWithStubImpl$Fragment$MathProblem$images<TRes>
+    implements CopyWith$Fragment$MathProblem$images<TRes> {
+  _CopyWithStubImpl$Fragment$MathProblem$images(this._res);
+
+  TRes _res;
+
+  call() => _res;
 }
