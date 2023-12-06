43a4fd5ff7a1bb9b0c454255b532057b7f9441b9
kume1a <kumela011@gmail.com>
2023-12-06 18:09:58 +0400

    export appclient types

--- lib/app_client.dart
+++ lib/app_client.dart
@@ -1,7 +1,17 @@
 library;
 
 export 'package:app_client/app_client.dart'
-    show AuthTokenStore, SecureStoreageTokenStoreImpl, NetworkClientFactory;
+    show
+        AuthTokenStore,
+        SecureStoreageTokenStoreImpl,
+        NetworkClientFactory,
+        RefreshTokenUsecase,
+        RefreshTokenUsecaseImpl,
+        ValidateAuthTokenUsecase,
+        ValidateAuthTokenUsecaseImpl,
+        SocketInstanceProvider,
+        SocketInstanceProviderImpl;
+
 export 'package:gql_types/gql_types.dart';
 
 export 'src/modules/index.dart';
