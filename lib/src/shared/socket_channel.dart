7be0bb52d119ec108685db78164b5aae46d25ea6
kume1a <kumela011@gmail.com>
2023-12-08 21:35:50 +0400

    remove logprint dep

--- lib/src/shared/socket_channel.dart
+++ lib/src/shared/socket_channel.dart
@@ -1,19 +1,17 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:meta/meta.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 
 import '../../app_client.dart';
-import 'typedefs.dart';
 
 abstract class SocketChannel<T extends Object?> {
   SocketChannel(
     this._socketInstanceProvider,
-    this._logPrint,
   );
 
   final SocketInstanceProvider _socketInstanceProvider;
-  final LogPrint _logPrint;
 
   @protected
   String get event;
@@ -43,12 +41,11 @@ abstract class SocketChannel<T extends Object?> {
 
   Future<void> _handleEvent(dynamic event) async {
     try {
-      _logPrint('event = $event');
       final T mapped = await map(event);
 
       _streamController.sink.add(mapped);
     } catch (e) {
-      _logPrint(e);
+      log('', error: e);
     }
   }
 
