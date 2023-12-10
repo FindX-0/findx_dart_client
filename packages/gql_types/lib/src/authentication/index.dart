7cc832a5df60861d8992e5d4aafb60f995119eb1
kume1a <kumela011@gmail.com>
2023-12-10 21:11:35 +0400

    fix admin sign in res type

--- packages/gql_types/lib/src/authentication/index.dart
+++ packages/gql_types/lib/src/authentication/index.dart
@@ -1,7 +1,8 @@
+import '../../gql_types.dart';
 import 'auth_payload_object.gql.dart';
 
 export 'admin_sign_in.gql.dart';
 export 'device_sign_in.gql.dart';
 
-typedef AdminSignInRes = Fragment$AuthPayload;
+typedef AdminSignInRes = Mutation$AdminSignIn$adminSignIn;
 typedef DeviceSignInRes = Fragment$AuthPayload;
