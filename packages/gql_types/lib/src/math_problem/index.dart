b0764d67f5f1a1d78b416788acf1320755c374b2
kume1a <kumela011@gmail.com>
2023-12-22 14:02:35 +0400

    export relations fragment

--- packages/gql_types/lib/src/math_problem/index.dart
+++ packages/gql_types/lib/src/math_problem/index.dart
@@ -1,3 +1,4 @@
 export 'math_problem.gql.dart';
+export 'math_problem_with_relations.gql.dart';
 export 'mutation/index.dart';
 export 'query/index.dart';
--- packages/gql_types/lib/src/math_sub_field/index.dart
+++ packages/gql_types/lib/src/math_sub_field/index.dart
@@ -1,3 +1,4 @@
 export 'math_sub_field.gql.dart';
+export 'math_sub_field_with_relations.gql.dart';
 export 'mutation/index.dart';
 export 'query/index.dart';
