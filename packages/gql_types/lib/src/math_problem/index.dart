65bad390e171598090dba06d00dce12f19ba7937
kume1a <kumela011@gmail.com>
2023-12-23 23:00:20 +0400

    export create math problem answer input

--- packages/gql_types/lib/src/math_problem/index.dart
+++ packages/gql_types/lib/src/math_problem/index.dart
@@ -1,4 +1,8 @@
+import '../../gql_types.dart';
+
 export 'math_problem.gql.dart';
 export 'math_problem_with_relations.gql.dart';
 export 'mutation/index.dart';
 export 'query/index.dart';
+
+typedef CreateMathProblemAnswerInput = Input$CreateMathProblemAnswerInput;
