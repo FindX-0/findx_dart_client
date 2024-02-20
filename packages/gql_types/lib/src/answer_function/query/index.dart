a57a091923bb988a6f395105c8b218fe86c17d59
kume1a <kumela011@gmail.com>
2024-02-21 00:19:51 +0400

    fix answer function page item typedef

--- packages/gql_types/lib/src/answer_function/query/index.dart
+++ packages/gql_types/lib/src/answer_function/query/index.dart
@@ -1,4 +1,5 @@
 import '../answer_function.gql.dart';
+import '../answer_function_with_relations.gql.dart';
 import 'filter_answer_functions.gql.dart';
 
 export 'filter_answer_functions.gql.dart';
@@ -8,5 +9,5 @@ export 'get_answer_function_by_id.gql.dart';
 typedef AnswerFunctionGetByIdRes = Fragment$AnswerFunction;
 
 typedef AnswerFunctionPage = Query$FilterAnswerFunctions$filterAnswerFunctions;
-typedef AnswerFunctionPageItem = Fragment$AnswerFunction;
+typedef AnswerFunctionPageItem = Fragment$AnswerFunctionWithRelations;
 typedef GetAllAnswerFunctionsItem = Fragment$AnswerFunction;
